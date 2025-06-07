759a0e53aa4daf8e7d9861f6b300a4ca24873640
Munday <derek.b.munday@outlook.com>
2025-06-07 14:12:37 +0000

    added pages to display legal content

---
+++ website/src/app/terms/page.tsx
@@ -0,0 +1,56 @@
+'use client'
+import * as React from 'react';
+import Container from '@mui/material/Container';
+import Box from '@mui/material/Box';
+import Stack from '@mui/material/Stack';
+import TermsAndConditions from '@/markdown/TermsAndConditions.md';
+import { styled } from '@mui/material/styles';
+
+export default function Page() {
+    return (
+        <Container maxWidth="lg">
+      <Box
+        sx={{
+          my: 4,
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Box
+      id="hero"
+      sx={(theme) => ({
+        width: '100%',
+        backgroundRepeat: 'no-repeat',
+
+        backgroundImage:
+          'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 90%), transparent)',
+        ...theme.applyStyles('dark', {
+          backgroundImage:
+            'radial-gradient(ellipse 80% 50% at 50% -20%, hsl(210, 100%, 16%), transparent)',
+        }),
+      })}
+    >
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          pt: { xs: 14, sm: 20 },
+          pb: { xs: 8, sm: 12 },
+        }}
+      >
+        <Stack
+          spacing={2}
+          useFlexGap
+          sx={{ alignItems: 'left', width: { xs: '100%', sm: '100%' } }}
+        >
+            <TermsAndConditions />
+        </Stack>
+    </Container>
+    </Box>
+      </Box>
+    </Container>
+    );
+}
